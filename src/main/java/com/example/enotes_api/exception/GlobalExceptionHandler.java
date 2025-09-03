7f48b0e34df7f90565f1080cb1a78fb18586f939
Prince Kumar <princebhagat12336@gmail.com>
2025-09-03 22:27:09 +0530

    Exception handling implemented

---
+++ src/main/java/com/example/enotes_api/exception/GlobalExceptionHandler.java
@@ -0,0 +1,28 @@
+package com.example.enotes_api.exception;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@Slf4j
+@ControllerAdvice  // @ControllerAdvice = A way to write centralized logic for all controllers.
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<?> handleException(Exception e){
+        return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @ExceptionHandler(NullPointerException.class)
+    public ResponseEntity<?> handleNullPointerException(Exception e){
+        return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<?> handleResourceNotFoundException(Exception e){
+        log.error("GlobalExceptionHandler : handleResourceNotFoundException() : {}", e.getMessage());
+        return new ResponseEntity<>(e.getMessage(), HttpStatus.NOT_FOUND);
+    }
+}
