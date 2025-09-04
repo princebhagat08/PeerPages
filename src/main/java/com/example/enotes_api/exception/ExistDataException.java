77a5bc58dd7dd01aed93817ef2befb470ef4bc02
Prince Kumar <princebhagat12336@gmail.com>
2025-09-05 00:08:27 +0530

    already exist category implemented

---
+++ src/main/java/com/example/enotes_api/exception/ExistDataException.java
@@ -0,0 +1,8 @@
+package com.example.enotes_api.exception;
+
+public class ExistDataException extends RuntimeException{
+
+    public ExistDataException(String message) {
+        super(message);
+    }
+}
--- src/main/java/com/example/enotes_api/repository/CategoryRepository.java
+++ src/main/java/com/example/enotes_api/repository/CategoryRepository.java
@@ -15,4 +15,5 @@ public interface CategoryRepository extends JpaRepository<Category,Integer> {
 
     Optional<Category> findByIdAndIsDeletedFalse(Integer id);
 
+   Boolean existsByName(String name);
 }
